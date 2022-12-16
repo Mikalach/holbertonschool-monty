d44d03f02fd1dfc4e30846644823a537f743f583
AsiaGrpr <groupierre.asia@gmail.com>
2022-12-16 06:26:49 -0800

    error code missed again to correct :/

--- func.c
+++ func.c
@@ -52,7 +52,7 @@ void add(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't add, stack too short\n", line_number);
-		return;
+		exit(EXIT_FAILURE);
 	}
 
 	temp = (*stack)->next;
