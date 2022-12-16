1643c8339c9bd459479ee55c1c48261937b60db6
AsiaGrpr <groupierre.asia@gmail.com>
2022-12-16 06:23:46 -0800

    error code missed to correct :/

--- func.c
+++ func.c
@@ -86,7 +86,7 @@ void _swap(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL || (*stack)->next == NULL)
 	{
 		fprintf(stderr, "L%d: can't swap, stack too short\n", line_number);
-		return;
+		exit(EXIT_FAILURE);
 	}
 	tmp = (*stack)->next;
 	(*stack)->next = tmp->next;
