c3a271b5b9a1282bd23909f46545fde7736d99d9
AsiaGrpr <groupierre.asia@gmail.com>
2022-12-16 06:17:06 -0800

    error code correction

--- pushpop.c
+++ pushpop.c
@@ -60,7 +60,7 @@ void _pop(stack_t **stack, unsigned int line_number)
 	if (*stack == NULL)
 	{
 		fprintf(stderr, "L%d: can't pop an empty stack\n", line_number);
-		return;
+		exit(EXIT_FAILURE);
 	}
 	tmp = *stack;
 	*stack = tmp->next;
